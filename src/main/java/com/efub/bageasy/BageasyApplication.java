8376252a40b0d45f425052aad0b5087bb54d424a
ParkIsComing <maroony55@gmail.com>
2023-07-01 12:40:57 +0900

    Initial commit

---
+++ src/main/java/com/efub/bageasy/BageasyApplication.java
@@ -0,0 +1,16 @@
+package com.efub.bageasy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+
+@EnableJpaAuditing
+@SpringBootApplication
+public class BageasyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BageasyApplication.class, args);
+	}
+
+}
