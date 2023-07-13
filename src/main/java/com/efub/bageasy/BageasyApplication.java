4a251e6483819a52f91f99e720cdbfd40da76104
YEJI <yeji.lee.kim@gmail.com>
2023-07-14 03:11:57 +0900

    chore: feign 관련 설정 삭제

--- src/main/java/com/efub/bageasy/BageasyApplication.java
+++ src/main/java/com/efub/bageasy/BageasyApplication.java
@@ -2,14 +2,11 @@ package com.efub.bageasy;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
-import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 
 @EnableJpaAuditing
 @SpringBootApplication()
-@EnableFeignClients
 public class BageasyApplication {
 
 	public static void main(String[] args) {
