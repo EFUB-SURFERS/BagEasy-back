00bc98c0500ebbf2c37fa35857f36e235fb7f5fc
nammsamm <ahtnwl1004@gmail.com>
2023-07-18 12:58:28 +0900

    fix : 응답으로 String형 school 을 받을 수 있도록 변경

--- src/main/java/com/efub/bageasy/domain/post/dto/PostListResponseDto.java
+++ src/main/java/com/efub/bageasy/domain/post/dto/PostListResponseDto.java
@@ -22,6 +22,7 @@ public class PostListResponseDto {
     private String postContent;
     private Long price;
     private Boolean isSold;
+    private String school;
     private Long buyerId;
     private LocalDateTime createdAt;
     private LocalDateTime modifiedAt;
@@ -35,6 +36,7 @@ public class PostListResponseDto {
         this.postContent=post.getContent();
         this.price=post.getPrice();
         this.isSold=post.getIsSold();
+        this.school=post.getSchool();
         this.buyerId=post.getBuyerId();
         this.createdAt=post.getCreatedAt();
         this.modifiedAt=post.getModifiedAt();
