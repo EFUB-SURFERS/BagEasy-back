31a08d702a1a8c95e065f0e795829e89236c9bcf
aqswa <joayoung3@gmail.com>
2023-07-22 07:36:42 +0900

    feat: 찜하기, 찜 해제 기능

---
+++ src/main/java/com/efub/bageasy/domain/heart/repository/HeartRepository.java
@@ -0,0 +1,12 @@
+package com.efub.bageasy.domain.heart.repository;
+
+import com.efub.bageasy.domain.heart.domain.Heart;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+
+public interface HeartRepository extends JpaRepository<Heart, Long>{
+    Optional<Heart> findByMemberIdAndPostId(Long memberId, Long postId);
+    Boolean existsHeartByMemberIdAndPostId(Long memberId, Long postId);
+}
---
+++ src/main/java/com/efub/bageasy/domain/heart/service/HeartService.java
@@ -0,0 +1,42 @@
+package com.efub.bageasy.domain.heart.service;
+
+import com.efub.bageasy.domain.heart.domain.Heart;
+import com.efub.bageasy.domain.heart.dto.HeartResponseDto;
+import com.efub.bageasy.domain.heart.repository.HeartRepository;
+import com.efub.bageasy.domain.member.domain.Member;
+import com.efub.bageasy.global.exception.CustomException;
+import com.efub.bageasy.global.exception.ErrorCode;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class HeartService {
+    public final HeartRepository heartRepository;
+
+    @Transactional
+    public void createByPostId(Member member, Long postId){
+        Heart heart = new Heart(member.getMemberId(), postId);
+        heartRepository.save(heart);
+    }
+
+    @Transactional
+    public void deleteByPostId(Member member, Long postId){
+        Heart heart = heartRepository.findByMemberIdAndPostId(member.getMemberId(), postId)
+                .orElseThrow(() -> new CustomException(ErrorCode.HEART_NOT_FOUND));
+        heartRepository.delete(heart);
+    }
+
+    @Transactional(readOnly = true)
+    public HeartResponseDto findExistence(Member member, Long postId){
+        if(heartRepository.existsHeartByMemberIdAndPostId(member.getMemberId(), postId)){
+            return new HeartResponseDto(true);
+        }
+        else{
+            return new HeartResponseDto(false);
+        }
+    }
+}
