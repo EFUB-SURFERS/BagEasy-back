f0f30f56ef17a32a7c9d6176a6b8390133b3ab3a
YEJI <yeji.lee.kim@gmail.com>
2023-07-27 00:01:58 +0900

    chore: 로그인, 토큰 재발급 시 응답에 memberId 제외

--- src/main/java/com/efub/bageasy/domain/member/dto/response/LoginResponseDto.java
+++ src/main/java/com/efub/bageasy/domain/member/dto/response/LoginResponseDto.java
@@ -9,7 +9,6 @@ public class LoginResponseDto {
 
     // DB에 있는 멤버인지
     private Boolean isExistingMember;
-    private Long memberId;
     private String email;
     private String nickname;
     private String accessToken;
@@ -17,7 +16,6 @@ public class LoginResponseDto {
     @Builder
     public LoginResponseDto(Member member, String accessToken, Boolean isExistingMember){
         this.isExistingMember = isExistingMember;
-        this.memberId = member.getMemberId();
         this.email = member.getEmail();
         this.nickname = member.getNickname();
         this.accessToken = accessToken;
--- src/main/java/com/efub/bageasy/domain/member/dto/response/ReissueResponseDto.java
+++ src/main/java/com/efub/bageasy/domain/member/dto/response/ReissueResponseDto.java
@@ -6,13 +6,11 @@ import lombok.Getter;
 @Getter
 public class ReissueResponseDto {
     private String accessToken;
-    private Long memberId;
     private String email;
     private String nickname;
 
     public ReissueResponseDto(String accessToken, Member member){
         this.accessToken = accessToken;
-        this.memberId = member.getMemberId();
         this.email = member.getEmail();
         this.nickname = member.getNickname();
     }
