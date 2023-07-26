8896deb336b9d4de959723a1953e00b439d557ea
Gayeong Park <maroony55@gmail.com>
2023-07-26 22:35:46 +0900

    fix : s3 region auto config 제외설정 삭제

--- src/main/java/com/efub/bageasy/BageasyApplication.java
+++ src/main/java/com/efub/bageasy/BageasyApplication.java
@@ -9,7 +9,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 @SpringBootApplication(exclude = {
 		org.springframework.cloud.aws.autoconfigure.context.ContextInstanceDataAutoConfiguration.class,
 		org.springframework.cloud.aws.autoconfigure.context.ContextStackAutoConfiguration.class,
-		org.springframework.cloud.aws.autoconfigure.context.ContextRegionProviderAutoConfiguration.class
+//		org.springframework.cloud.aws.autoconfigure.context.ContextRegionProviderAutoConfiguration.class
 }
 )
 public class BageasyApplication {
